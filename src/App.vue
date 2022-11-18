aa63c81395dedae0498b26d97b9aa6d1bd5b74f7
Nicolas Couraud <sp.couraud.nicolas@gmail.com>
2022-11-18 19:07:41 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="array-vi">
+    <ArrayVisualizer :arr="testArray"> </ArrayVisualizer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ArrayVisualizer from "./components/ArrayVisualizer.vue";
+const testArray = [1, 2, 3, 4, 8];
+</script>
+
+<style scoped>
+.array-vi {
+  width: 400px;
+}
+</style>
---
+++ src/components/ArrayVisualizer.vue
@@ -0,0 +1,44 @@
+<template>
+  <h2>Array Visualizer</h2>
+  <div class="array-container">
+    <div v-for="(el, ind) in arr" class="array-el">
+      <div class="indice">{{ind}}</div>
+      <div class="internal-data">{{el}}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+defineProps({
+  arr: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.array-container {
+  display: flex;
+}
+.array-el {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 80px;
+    height: 80px;
+    border: 1px solid black;
+    margin: 10px;
+}
+.indice{
+    position: absolute;
+    left:0;
+    top: 0;
+}
+.internal-data{
+    margin:auto;
+    text-align:center;    
+}
+</style>
